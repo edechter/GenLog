55d3d1af037c6f71217e3abb9144a759b317aced
Eyal Dechter <eyaldechter@gmail.com>
2015-05-07 13:21:57 -0400

    Major changes to search algorithm.
    The beam partial derivation weights are now calculated according to the
    generalized prefix conditional probability of the derivation. This means
    that the scores of the partial derivations on the beam can change as the
    search progresses, so it no longer makes sense to store the beam as a
    priority queue, though I haven't made that change yet.

--- src/scratch2.pl
+++ src/scratch2.pl
@@ -17,9 +17,9 @@ number_sentences(Xs) :-
               'one two three' - 100,
               'one two three four' - 100,
               'one two three four five' - 100,
-              'one two three four five six' - 10,
-              'one two three four five six seven' - 10
-              % 'one two three four five six seven eight nine ten eleven' - 10
+              'one two three four five six' - 100,
+              'one two three four five six seven' - 100,
+              'one two three four five six seven eight nine ten eleven' - 100
               ].
 
 count_sentence(Lo, Hi, X) :-
@@ -32,31 +32,28 @@ count_sentence(Lo, Hi, X) :-
         atomic_list_concat(Ws, ' ', X).
 
 number_sentences1(Xs) :-
-        findall(X-10,
-                (between(1, 20, N),
+        findall(X-100,
+                (between(1, 10, N),
                  count_sentence(1, N, X)),
                 Xs).
         
 goal(Goal) :-
-        number_sentences(Xs),
+        number_sentences1(Xs),
         member(X-C, Xs),
         atomic_list_concat(Ws, ' ', X),
-        T = hear(Ws-[]|[]-[]),
+        T = hear(Ws-[]),
         Goal = count(T,C).
                       
 goals(Goals) :-
         findall(G, goal(G),
                  Goals).
         
-
 go :-
-        compile_sdcl_file('../example/lpn.gl'),
-        Options = [beam_width(10000), time_limit_seconds(1)],
+        compile_sdcl_file('test.gl'),
+        Options = [beam_width(100), time_limit_seconds(4)],
         set_rule_alphas(uniform),
-        number_sentences(Xs),
         goals(Goals),
         % sentence_data_set(hear, Xs, Goals),
         writeln(Goals),
-        (compile:show_rules;true),
         run_batch_vbem(Goals, Options).
   
