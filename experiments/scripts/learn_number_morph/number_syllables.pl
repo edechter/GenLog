3016f8170606a15d1e0cb4725db8b692bc06f7c7
Eyal Dechter <eyaldechter@gmail.com>
2015-05-12 11:52:25 -0400

    Added new files for running number morphology experiment.

---
+++ experiments/scripts/learn_number_morph/number_syllables.pl
@@ -0,0 +1,107 @@
+%% syllables for number words between 1 and 100
+%% generated using the CMU pronunciation dictionay (cmudict-0.7b)
+%% and python package syllabify (https://github.com/kylebgorman/syllabify)
+
+%% number_syllables(Number, [Word])
+%% Each word is a list of syllables.
+%% Each syllable is a triple (onset, nucleus, coda)
+
+number_syllables(1, [[(['W'], ['AH1'], ['N'])]]).
+number_syllables(2, [[(['T'], ['UW1'], [])]]).
+number_syllables(3, [[(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(4, [[(['F'], ['AO1'], ['R'])]]).
+number_syllables(5, [[(['F'], ['AY1'], ['V'])]]).
+number_syllables(6, [[(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(7, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(8, [[([], ['EY1'], ['T'])]]).
+number_syllables(9, [[(['N'], ['AY1'], ['N'])]]).
+number_syllables(10, [[(['T'], ['EH1'], ['N'])]]).
+number_syllables(11, [[([], ['IH0'], []), (['L'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(12, [[(['T', 'W'], ['EH1'], ['L', 'V'])]]).
+number_syllables(13, [[(['TH'], ['ER1'], []), (['T'], ['IY1'], ['N'])]]).
+number_syllables(14, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY1'], ['N'])]]).
+number_syllables(15, [[(['F'], ['IH0'], ['F']), (['T'], ['IY1'], ['N'])]]).
+number_syllables(16, [[(['S'], ['IH0'], ['K']), (['S', 'T'], ['IY1'], ['N'])]]).
+number_syllables(17, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY1'], ['N'])]]).
+number_syllables(18, [[([], ['EY0'], []), (['T'], ['IY1'], ['N'])]]).
+number_syllables(19, [[(['N'], ['AY1'], ['N']), (['T'], ['IY1'], ['N'])]]).
+number_syllables(20, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])]]).
+number_syllables(21, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(22, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(23, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(24, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(25, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(26, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(27, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(28, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(29, [[(['T', 'W'], ['EH1'], ['N']), (['T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(30, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])]]).
+number_syllables(31, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(32, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(33, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(34, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(35, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(36, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(37, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(38, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(39, [[(['TH'], ['ER1'], []), (['D'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(40, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])]]).
+number_syllables(41, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(42, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(43, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(44, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(45, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(46, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(47, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(48, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(49, [[(['F'], ['AO1', 'R'], []), (['T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(50, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])]]).
+number_syllables(51, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(52, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(53, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(54, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(55, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(56, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(57, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(58, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(59, [[(['F'], ['IH1'], ['F']), (['T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(60, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])]]).
+number_syllables(61, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(62, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(63, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(64, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(65, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(66, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(67, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(68, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(69, [[(['S'], ['IH1'], ['K']), (['S', 'T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(70, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])]]).
+number_syllables(71, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(72, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(73, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(74, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(75, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(76, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(77, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(78, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(79, [[(['S'], ['EH1'], []), (['V'], ['AH0'], ['N']), (['T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(80, [[([], ['EY1'], []), (['T'], ['IY0'], [])]]).
+number_syllables(81, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(82, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(83, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(84, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(85, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(86, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(87, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(88, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(89, [[([], ['EY1'], []), (['T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
+number_syllables(90, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])]]).
+number_syllables(91, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['W'], ['AH1'], ['N'])]]).
+number_syllables(92, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['T'], ['UW1'], [])]]).
+number_syllables(93, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['TH', 'R'], ['IY1'], [])]]).
+number_syllables(94, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['F'], ['AO1'], ['R'])]]).
+number_syllables(95, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['F'], ['AY1'], ['V'])]]).
+number_syllables(96, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['S'], ['IH1'], ['K', 'S'])]]).
+number_syllables(97, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['S'], ['EH1'], []), (['V'], ['AH0'], ['N'])]]).
+number_syllables(98, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [([], ['EY1'], ['T'])]]).
+number_syllables(99, [[(['N'], ['AY1'], ['N']), (['T'], ['IY0'], [])], [(['N'], ['AY1'], ['N'])]]).
---
+++ experiments/scripts/learn_number_morph/runner.pl
@@ -0,0 +1,90 @@
+
+
+:- multifile user:file_search_path/2.
+:- dynamic   user:file_search_path/2.
+
+:- getenv('GENLOG_DIR', Dir),
+   asserta(file_search_path(genlog, Dir));
+   true.
+   
+        
+%% ----------------------------------------------------------------------
+
+:- use_module(library(lists)).
+
+:- use_module(genlog(experiment)).
+
+:- use_module(genlog(sdcl)).
+:- use_module(genlog(compile)).
+
+:- use_module(genlog(learn)).
+:- use_module(genlog(pgen)).
+:- use_module(genlog(pprint)).
+
+:- use_module(genlog(data_utils)).
+:- use_module(genlog(number_words)).
+
+:- [number_syllables].
+
+%% this file
+:- absolute_file_name('./syllables.pl', Abs),
+   set_setting(experiment:runner, Abs).
+
+%% experiment data directory
+:- set_setting(experiment:root, '../data').
+
+
+%% ------------------------------------------
+%% make number data
+number_goal(N, Count, Goal) :-
+        number_phones(N, Phones),
+        Goal = count(hear(Phones-[]), Count).
+
+number_goals(Lo, Hi, Count, Goals) :-
+        findall(Goal,
+                (between(Lo, Hi, N),
+                 number_goal(N, Count, Goal)),
+                Goals).
+
+number_phones(N, Phones) :-
+       number_syllables(N, Syls), % Syls is list of words, each one is list of triples
+       flatten(Syls, Syls1),      % Syls1 is a list of triples
+       findall(P,
+               (
+                member(Trip, Syls1),
+                (Trip = (P, _, _);
+                 Trip = (_, P, _);
+                 Trip = (_, _, P))
+               ),
+               Phones).
+
+number_phone_lexicon(Phones) :-
+        findall(P,
+                (between(1, 99, N),
+                 number_phones(N, Xs),
+                 member(P, Xs)),
+                Ps),
+        sort(Ps, Phones).
+
+number_phone(Phone) :-
+        number_phone_lexicon(Phones),
+        member(Phone, Phones).
+        
+
+
+gl_file('../../gls/number_morph.gl').
+        
+main :-
+        % experiment:setup_experiment,
+        gl_file(GlFile),
+        compile_sdcl_file(GlFile),
+        Options = [beam_width(100), time_limit_seconds(6)],
+        set_rule_alphas(uniform),
+        number_goals(1, 99, 1, Goals),
+        list_to_random_choice(Goals, GoalGen),
+        writeln(GoalGen),
+        run_online_vbem(GoalGen, Data, Options),        
+        writeln(Data).
+  
+
+        
