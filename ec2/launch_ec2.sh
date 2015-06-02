f8e4a76d5ad3451a9856f45ea1d7f9f0cd33220e
Eyal Dechter <eyaldechter@gmail.com>
2015-06-02 14:10:43 -0400

    Enabled email notifications and launch script.

--- ec2/launch_ec2.sh
+++ ec2/launch_ec2.sh
@@ -8,19 +8,35 @@ INSTANCE_TYPE=m1.small
 USERDATA_SCRIPT_IN=setup_genlog.sh.in
 USERDATA_SCRIPT=setup_genlog.sh
 SPOT_BID=0.02
-INSTANCE_COUNT=2
+INSTANCE_COUNT=1
 KEY_PAIR=ec2
+# Should we use an on-demand instance (0 for spot instance)
+ON_DEMAND=1
 
-sed "s-<AWS_ACCESS_KEY>-${AWS_ACCESS_KEY}-g; s-<AWS_SECRET_KEY>-${AWS_SECRET_KEY}-g;" \
+SUBST="s-<AWS_ACCESS_KEY>-${AWS_ACCESS_KEY}-g;" 
+SUBST="$SUBST s-<AWS_SECRET_KEY>-${AWS_SECRET_KEY}-g;"
+SUBST="$SUBST s-<GMAIL_USER>-${GMAIL_USER}-g;"
+SUBST="$SUBST s-<GMAIL_PASSWORD>-${GMAIL_PASSWORD}-g;"
+sed $SUBST
     ${USERDATA_SCRIPT_IN} > ${USERDATA_SCRIPT}
 
-ec2-request-spot-instances \
+
+if [ $ON_DEMAND == 1 ] 
+then
+    CMD=ec2-run-instances
+    PRICE_OPT=""
+else
+    CMD=ec2-request-spot-instances
+    PRICE_OPT="--price $SPOT_BID"
+fi
+
+$CMD \
     ${IMAGE_ID} \
-    --price ${SPOT_BID} \
     --instance-count ${INSTANCE_COUNT} \
     --user-data-file ${USERDATA_SCRIPT} \
     --instance-type ${INSTANCE_TYPE} \
-    --key ${KEY_PAIR}
+    --key ${KEY_PAIR} \
+    ${PRICE_OPT}
 
 
     
