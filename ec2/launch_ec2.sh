d1d55ca09523dfa4617dc49432081a0b5ad0cec2
Eyal Dechter <eyaldechter@gmail.com>
2015-06-02 16:40:53 -0400

    Fixed bugs in setup_genlog.sh.in.
    - Testing on an ec2 instance.

--- ec2/launch_ec2.sh
+++ ec2/launch_ec2.sh
@@ -17,26 +17,26 @@ SUBST="s-<AWS_ACCESS_KEY>-${AWS_ACCESS_KEY}-g;"
 SUBST="$SUBST s-<AWS_SECRET_KEY>-${AWS_SECRET_KEY}-g;"
 SUBST="$SUBST s-<GMAIL_USER>-${GMAIL_USER}-g;"
 SUBST="$SUBST s-<GMAIL_PASSWORD>-${GMAIL_PASSWORD}-g;"
-sed $SUBST
+sed "$SUBST" \
     ${USERDATA_SCRIPT_IN} > ${USERDATA_SCRIPT}
 
 
-if [ $ON_DEMAND == 1 ] 
-then
-    CMD=ec2-run-instances
-    PRICE_OPT=""
-else
-    CMD=ec2-request-spot-instances
-    PRICE_OPT="--price $SPOT_BID"
-fi
+# if [ $ON_DEMAND == 1 ] 
+# then
+#     CMD=ec2-run-instances
+#     PRICE_OPT=""
+# else
+#     CMD=ec2-request-spot-instances
+#     PRICE_OPT="--price $SPOT_BID"
+# fi
 
-$CMD \
-    ${IMAGE_ID} \
-    --instance-count ${INSTANCE_COUNT} \
-    --user-data-file ${USERDATA_SCRIPT} \
-    --instance-type ${INSTANCE_TYPE} \
-    --key ${KEY_PAIR} \
-    ${PRICE_OPT}
+# $CMD \
+#     ${IMAGE_ID} \
+#     --instance-count ${INSTANCE_COUNT} \
+#     --user-data-file ${USERDATA_SCRIPT} \
+#     --instance-type ${INSTANCE_TYPE} \
+#     --key ${KEY_PAIR} \
+#     ${PRICE_OPT}
 
 
     
