07044dce70264c608778749d358143d4f2c3be87
Eyal Dechter <eyaldechter@gmail.com>
2015-06-02 12:04:28 -0400

    Removed AWS keys from setup_genlog.sh.
    - Using sed replacement to insert AWS keys into setup_genlog.sh.in.

--- ec2/launch_ec2.sh
+++ ec2/launch_ec2.sh
@@ -1,14 +1,18 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 #
 # ----------------------------------------------------------------------
 set -x
 
 IMAGE_ID=ami-765b3e1f
 INSTANCE_TYPE=m1.small
+USERDATA_SCRIPT_IN=setup_genlog.sh.in
 USERDATA_SCRIPT=setup_genlog.sh
 SPOT_BID=0.02
-INSTANCE_COUNT=1
-KEY_PAIR=eyal_ec2
+INSTANCE_COUNT=2
+KEY_PAIR=ec2
+
+sed "s-<AWS_ACCESS_KEY>-${AWS_ACCESS_KEY}-g; s-<AWS_SECRET_KEY>-${AWS_SECRET_KEY}-g;" \
+    ${USERDATA_SCRIPT_IN} > ${USERDATA_SCRIPT}
 
 ec2-request-spot-instances \
     ${IMAGE_ID} \
