e789c439e9eb769d9ea5d419d65f51a6c7f4ae3b
Eyal Dechter <eyaldechter@gmail.com>
2015-06-22 12:17:23 -0400

    Minor changes.

--- ec2/init_genlog.sh
+++ ec2/init_genlog.sh
@@ -12,18 +12,21 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console)
 sudo -i -u edechter bash -c <<'EOF'
 # HACK: fake being an interactive script so that ~/.bashrc executes
 export PS1=1
+
 # source .bashrc
 source $HOME/.bashrc
 
 echo "Pulling from GenLog repo..."
 cd $GENLOG_ROOT
-git pull origin master
+git fetch --all
+git reset --hard origin/master
 cd -
 echo "Done."
 
 echo "Running run_genlog.sh..."
 cd $GENLOG_ROOT/ec2
 ./run_genlog.sh
+cd -
 echo "Done."
 
 EOF
