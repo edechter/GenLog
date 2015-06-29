79c444868320a24206be404ced02c8fb445b5df5
Eyal Dechter <eyaldechter@gmail.com>
2015-06-29 11:42:36 -0400

    Set boot.sh to execute command passed to docker run.

--- docker/GenLog/launch_job.sh
+++ docker/GenLog/launch_job.sh
@@ -6,21 +6,13 @@
 JOB_ID=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
 S3_URL='s3://edechter.genlog'
-GL_JOB_ID=1435093342217
-
-# # create a docker machine
-# docker-machine create \
-#     --driver virtualbox \
-#     genlog-dev
-
-# load machine enviroment
-eval "$(docker-machine env genlog-dev)"
-
+# GL_JOB_ID=1435093342217
 
 # run 
 docker run \
     --env-file env_file \
     -i \
     -t edechter/genlog  \
-    # bash
-    '/home/genlog/GenLog/ec2/job.sh' "$JOB_ID" "$GL_JOB_ID" 1 1 2
+    '/home/genlog/GenLog/ec2/job.sh' "$JOB_ID" "$@"
+
+
