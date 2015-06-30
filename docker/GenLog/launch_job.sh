adee402faa82d5c14ad7768983c23be634c61410
Eyal Dechter <eyaldechter@gmail.com>
2015-06-30 14:01:37 -0400

    Fixed use of parallel in run.sh.

--- docker/GenLog/launch_job.sh
+++ docker/GenLog/launch_job.sh
@@ -10,9 +10,9 @@ S3_URL='s3://edechter.genlog'
 # run 
 docker run \
     --env-file env_file \
-    -v $(pwd):/home/genlog/data \
-    -v $(pwd):/home/genlog/logs \
-    -it edechter/genlog  \
+    -v $(pwd)/data:/home/genlog/data \
+    -v $(pwd)/logs:/home/genlog/logs \
+    edechter/genlog  \
     '/home/genlog/GenLog/ec2/job.sh' "$JOB_ID" "$@"
 
 
--- docker/GenLog/run.sh
+++ docker/GenLog/run.sh
@@ -4,16 +4,15 @@
 
 
 
-NUMBERS="$(seq 1 2)"
+NUMBERS="$(seq 1 10)"
 GL_JOB_ID='1435093342217'
-GL_ITERS="$(seq 1 1 1)"
+GL_ITERS="$(seq 1 1 2)"
+JOBS_PER_GL=2
 
-
-parallel -X -j1 echo {} ::: "$GL_ITERS" | \
+parallel -X -j"$JOBS_PER_GL" echo {} ::: "$NUMBERS" | \
     parallel \
-    --dry-run \
-       -j10 -a - \
+       -a - \
       --joblog /tmp/genlog.log \
       ./launch_job.sh "$GL_JOB_ID" \
                        '{2} {1}' \
-    ::: "$NUMBERS"
+    ::: "$GL_ITERS"
