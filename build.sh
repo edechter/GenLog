1cb9a539fb8b74dfd93e76251c1e42160ca4b536
Eyal Dechter <eyaldechter@gmail.com>
2015-09-16 17:22:22 -0400

    Modified build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -ex
 
+cd ./src
+
 CPATH="$HOME"/Dropbox/Projects/swipl-all/swipl-devel/include/
 LIBRARY_PATH="$HOME"/Dropbox/Projects/swipl-all/swipl-devel/lib/x86_64-darwin13.2.0/
+
+mkdir -p ../lib 
 
 gcc -I"$CPATH" \
    -L"$LIBRARY_PATH" \
@@ -19,3 +23,4 @@ gcc -v -shared -W1 -o ../lib/multinom.dylib multinom.o \
    -L/"$LIBRARY_PATH" \
    -lswipl
 
+cd -
